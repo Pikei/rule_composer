049fa521520600a4545ec91ba91185c7911083ee
pikei <piotrkarol2602@gmail.com>
2026-03-14 22:57:19 +0100

    added core dependencies

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+ * \file main.cpp
+ * Created by Piotr Karol 2026
+ */
+
+#include <iostream>
+
+int main(const int argc, const char * argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
